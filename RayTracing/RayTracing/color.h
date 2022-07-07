cd59fe674fa5d95ef2b6210d3865fcca5be2413e
sxl <1097166568@qq.com>
2022-07-07 18:00:27 +0800

    3.3

---
+++ RayTracing/RayTracing/color.h
@@ -0,0 +1,14 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+#include<iostream>
+
+void write_color(std::ostream& out, color pixel_color) {
+	// 寫下每個顯色分量轉換後的值[0, 255]
+	out << static_cast<int>(255.99 * pixel_color.x()) << ' '
+		<< static_cast<int>(255.99 * pixel_color.y()) << ' '
+		<< static_cast<int>(255.99 * pixel_color.z()) << '\n';
+}
+
+#endif // !COLOR_H
--- RayTracing/RayTracing/main.cpp
+++ RayTracing/RayTracing/main.cpp
@@ -1,4 +1,8 @@
 #include<iostream>
+#include "vec3.h"
+#include "color.h"
+using std::cin;
+using std::cout;
 
 int main() 
 {
@@ -16,15 +20,11 @@ int main()
 	{
 		for (int i = 0; i < image_width; ++i)
 		{
-			auto r = double(i) / (image_width - 1);
-			auto g = double(j) / (image_height - 1);
-			auto b = 0.25;
-
-			int ir = static_cast<int>(255.99 * r);
-			int ig = static_cast<int>(255.99 * g);
-			int ib = static_cast<int>(255.99 * b);
-
-			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+			double r = double(i) / (image_width - 1);
+			double g = double(j) / (image_height - 1);
+			double b = 0.25;
+			color pixel_color(r, g, b);
+			write_color(cout, pixel_color);
 		}
 	}
 
